4b6e696e5090cbe4d318da317f06a2541f83b608
Mazhar <2561458+Mazharul-Hossain@users.noreply.github.com>
2025-06-10 17:25:48 -0500

    end-of-life: retainable

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -24,32 +24,12 @@ export class BlogsComponent {
         const winRef = this.windowRef.nativeWindow();
 
         if (winRef) {
-            //We loading the script on after view is loaded
-            // this.loadScripts();
-
             // Fetch RSS feed
             this.fetchRSSFeed(this.rssUrl).then(items => {
                 this.populateRSSFeed(items);
             }).catch(error => {
                 console.error('Error in ngAfterViewInit while fetching RSS feed:', error);
             });
-        }
-    }
-
-    // Method to dynamically load JavaScript
-    loadScripts() {
-        // This array contains all the files/CDNs
-        // https://www.geeksforgeeks.org/how-to-load-external-js-scripts-dynamically-in-angularjs/
-        const dynamicScripts = [
-            "assets/js/retainable-rss-embed.js"
-        ];
-
-        for (let i = 0; i < dynamicScripts.length; i++) {
-            const node = document.createElement('script');
-            node.src = dynamicScripts[i];
-            node.type = 'text/javascript';
-            node.async = true;
-            document.getElementsByTagName('body')[0].appendChild(node);
         }
     }
 
