7fb4017ff888abae31790066948d66413e837ee9
Mazhar <2561458+Mazharul-Hossain@users.noreply.github.com>
2025-08-05 15:33:32 -0500

    Update gtag.service.ts

--- src/app/gtag/gtag.service.ts
+++ src/app/gtag/gtag.service.ts
@@ -9,23 +9,21 @@ export class GtagService {
     constructor(@Inject(DOCUMENT) private document: Document) { }
 
     addGtagScript(): void {
-        // Google Tag Manager
+        // Google Tag Manager (gtag.js)
         const inlineScript = this.document.createElement('script');
         inlineScript.text = `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WNQ3WGZ');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CYRKWCGZNN');
         `;
         this.document.head.insertBefore(inlineScript, this.document.head.firstChild);
 
-        // Google Tag Manager (noscript)
-        const anotherInlineScript = this.document.createElement('noscript');
-        inlineScript.text = `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNQ3WGZ"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        `;
-        this.document.body.insertBefore(anotherInlineScript, this.document.body.firstChild);
+        const anotherInlineScript = this.document.createElement('script');
+        anotherInlineScript.src = "https://www.googletagmanager.com/gtag/js?id=G-CYRKWCGZNN";
+        anotherInlineScript.async = true; // makes script run asynchronously
+        anotherInlineScript.type = "text/javascript"; // set the script type
+        this.document.head.insertBefore(anotherInlineScript, this.document.head.firstChild);
     }
 }
